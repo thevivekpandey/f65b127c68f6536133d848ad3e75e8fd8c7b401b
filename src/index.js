cbc5ecf3b05335c3567a476288d616ab5c2da33e
thevivekpandey <thevivekpandey@gmail.com>
2016-12-28 23:37:39 +0530

    Switch to prod site

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Button from "./components/button";
 import Headers from "./components/headers";
 import Output from "./components/output";
 
-const SERVER_BASE_URL = "http://127.0.0.1:8000/run"
+const SERVER_BASE_URL = "http://www.f65b127c68f6536133d848ad3e75e8fd8c7b401b.run/run"
 class App extends Component {
   constructor(props) {
     super(props);
